ad4680a341b8bd4f97e74e6b5566de0164e2bc1b
jakosimov <jakobalwall@gmail.com>
2021-07-04 15:03:49 +0200

    Make Client and methods public

--- src/client_api/client.rs
+++ src/client_api/client.rs
@@ -5,31 +5,31 @@ use crate::transactions::{
     UnsignedLicensePurchase, UnsignedLicenseTransfer, UnsignedSelfListing,
 };
 
-const TRANSACTIONS_TOPIC: &str = "transactions";
+pub const TRANSACTIONS_TOPIC: &str = "transactions";
 
 #[allow(dead_code)]
-struct Client {
+pub struct Client {
     private: PrivateKey,
     network: Network,
 }
 
 #[allow(dead_code)]
 impl Client {
-    async fn new(private: PrivateKey) -> Self {
+    pub async fn new(private: PrivateKey) -> Self {
         Client {
             private,
             network: network::create(TRANSACTIONS_TOPIC).await.unwrap(),
         }
     }
 
-    async fn send_transaction(&mut self, transaction: Transaction) {
+    pub async fn send_transaction(&mut self, transaction: Transaction) {
         // Serialization should probably be changed to some other format later,
         // but json might be easier to debug for now.
         let message = serde_json::to_string(&transaction).unwrap();
         self.network.broadcast(message.as_bytes()).await.unwrap();
     }
 
-    async fn make_currency_transfer(&mut self, amount: u64, recipient: UserId) {
+    pub async fn make_currency_transfer(&mut self, amount: u64, recipient: UserId) {
         let transfer = self
             .private
             .sign(UnsignedCurrencyTransfer { amount, recipient });
@@ -37,25 +37,25 @@ impl Client {
             .await;
     }
 
-    async fn make_self_listing(&mut self, price: u64) {
+    pub async fn make_self_listing(&mut self, price: u64) {
         let listing = self.private.sign(UnsignedSelfListing { price });
         self.send_transaction(Transaction::SelfListing(listing))
             .await;
     }
 
-    async fn make_license_order(&mut self, seller: UserId, price: u64) {
+    pub async fn make_license_order(&mut self, seller: UserId, price: u64) {
         let order = self.private.sign(UnsignedLicenseOrder { seller, price });
         self.send_transaction(Transaction::LicenseOrder(order))
             .await;
     }
 
-    async fn make_license_listing(&mut self, license: LicenseId, price: u64) {
+    pub async fn make_license_listing(&mut self, license: LicenseId, price: u64) {
         let listing = self.private.sign(UnsignedLicenseListing { license, price });
         self.send_transaction(Transaction::LicenseListing(listing))
             .await;
     }
 
-    async fn make_license_purchase(&mut self, seller: UserId, license: LicenseId, price: u64) {
+    pub async fn make_license_purchase(&mut self, seller: UserId, license: LicenseId, price: u64) {
         let purchase = self.private.sign(UnsignedLicensePurchase {
             seller,
             license,
@@ -65,7 +65,7 @@ impl Client {
             .await;
     }
 
-    async fn make_license_transfer(&mut self, license: LicenseId, recipient: UserId) {
+    pub async fn make_license_transfer(&mut self, license: LicenseId, recipient: UserId) {
         let transfer = self
             .private
             .sign(UnsignedLicenseTransfer { license, recipient });
