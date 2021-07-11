8b9c6a8c2cb81e46f3a25d16b7e54212417c2c22
Miles Watson <contact@mileswatson.net>
2021-07-11 20:16:10 +0100

    Removed invalid future impl.

--- src/consensus/mod.rs
+++ src/consensus/mod.rs
@@ -1,54 +1,45 @@
 use crate::crypto::hashing::Hashable;
-use std::{
-    future::Future,
-    pin::Pin,
-    task::{Context, Poll},
-};
-
 mod app;
 mod types;
 
 pub use app::*;
 pub use types::*;
 
-struct Tendermint<A: App<B>, B: Hashable> {
+struct Tendermint<A: App<B>, B: Hashable + Clone> {
     height: u64,
     round: u64,
-    step: Option<Step>,
+    step: Step,
     locked: Option<(u64, B)>,
     valid: Option<(u64, B)>,
     app: A,
 }
 
-impl<A: App<B>, B: Hashable> Tendermint<A, B> {
+impl<A: App<B>, B: Hashable + Clone> Tendermint<A, B> {
     fn new(app: A) -> Self {
         Tendermint {
             height: 0,
             round: 0,
-            step: None,
+            step: Step::Propose,
             locked: None,
             valid: None,
             app,
         }
     }
 
-    /*fn start_round(&mut self, round: u64) {
+    async fn run(&mut self) {
+        self.start_round(0);
+    }
+
+    async fn start_round(&mut self, round: u64) {
         self.round = round;
         self.step = Step::Propose;
         if self.app.proposer(self.round) == self.app.id() {
-            match valid {
-                Some((round, value)) =>
-            }
+            let proposal = match self.valid.take() {
+                Some((round, value)) => (Some(round), value),
+                None => (None, self.app.create_block()),
+            };
         } else {
             // Schedule!
         }
-    }*/
-}
-
-impl<A: App<B>, B: Hashable> Future for Tendermint<A, B> {
-    type Output = Broadcast<B>;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Broadcast<B>> {
-        todo!()
     }
 }
