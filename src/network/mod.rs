b52c812a04278e878c0c8b778ba9de894f1989be
Miles Watson <contact@mileswatson.net>
2021-09-15 11:43:45 +0100

    Added function to connect network and app.

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -4,7 +4,7 @@ pub mod p2p;
 use std::error::Error;
 
 use async_trait::async_trait;
-use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 #[async_trait(?Send)]
 pub trait Network<M> {
@@ -15,4 +15,17 @@ pub trait Network<M> {
 #[async_trait(?Send)]
 pub trait Node<M> {
     async fn run(&mut self, incoming: Sender<M>, mut outgoing: Receiver<M>) -> Result<(), ()>;
+}
+
+async fn connect<M>(mut app: impl Node<M>, mut network: impl Node<M>) -> Result<(), ()> {
+    let (s1, r1) = channel(10);
+    let (s2, r2) = channel(10);
+
+    let (app_res, network_res) = tokio::join!(app.run(s1, r2), network.run(s2, r1));
+
+    if app_res.is_ok() && network_res.is_ok() {
+        Ok(())
+    } else {
+        Err(())
+    }
 }
